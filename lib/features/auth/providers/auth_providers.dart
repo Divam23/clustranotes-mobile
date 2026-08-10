da0cc7ad66bfd6c7e21db64b2492176ec996157a
Divam Dubey <134174769+Divam23@users.noreply.github.com>
2026-08-10 23:10:15 +0530

    Refactor authentication and upload features
    - Removed MainActivity.kt as it is no longer needed.
    - Added login_screen.dart for user login functionality.
    - Replaced signup_page.dart with signup_screen.dart for improved signup UI.
    - Deleted auth_provider.dart and introduced auth_providers.dart to manage authentication providers.
    - Implemented NoteMetadataDropdown and NoteMetadataTextField widgets for better note metadata input.
    - Created upload_notifier.dart and upload_state.dart to manage upload state and logic.
    - Deleted firebase_options.dart as it is no longer required.

---
+++ lib/features/auth/providers/auth_providers.dart
@@ -0,0 +1,21 @@
+import 'package:clustranotes_mobile/core/firebase/provider/firebase_auth_provider.dart';
+import 'package:clustranotes_mobile/core/firebase/provider/google_sign_in_provider.dart';
+import 'package:clustranotes_mobile/features/auth/data/datasource/auth_remote_data_source.dart';
+import 'package:clustranotes_mobile/features/auth/data/repository/auth_repository_impl.dart';
+import 'package:clustranotes_mobile/features/auth/domain/repositories/auth_repository.dart';
+import 'package:clustranotes_mobile/features/auth/notifier/auth_notifier.dart';
+import 'package:clustranotes_mobile/features/auth/notifier/auth_state.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
+
+final authRemoteDataSourceProvider = Provider<AuthRemoteDataSource>((ref){
+  return AuthRemoteDataSource(ref.read(firebaseAuthProvider), ref.read(googleSignInProvider));
+}); 
+
+final authRepositoryProvider = Provider<AuthRepository>((ref){
+  return AuthRepositoryImpl(ref.read(authRemoteDataSourceProvider));
+});
+
+final authNotifierProvider = StateNotifierProvider<AuthNotifier, AuthState>((ref){
+  return AuthNotifier(ref.read(authRepositoryProvider));
+});
